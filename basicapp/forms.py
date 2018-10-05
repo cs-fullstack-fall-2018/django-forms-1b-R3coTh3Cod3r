23d2731a3861cc73c1a21bf8f356b84162e6c081
Mareco Nabors <naborsmarico99@gmail.com>
2018-10-05 15:13:31 -0500

    djangoForms1

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -3,5 +3,8 @@ from django import forms
 
 class FormName(forms.Form):
     name = forms.CharField()
+    url = forms.CharField(widget=forms.URLInput)
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+
+
